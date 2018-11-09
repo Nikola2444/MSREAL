3d54ddb4c84ce560ba7da8652cfbec44678caef3
nikola <nikola2444@yahoo.com>
2018-11-09 15:39:27 +0100

    vezba3 added to rep

---
+++ hello_driver/hello_driver.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h>
+MODULE_LICENSE("Dual BSD/GPL"); //govori da modul koristi besplatnu licencu
+
+static int __init hello_init(void)
+{
+   printk(KERN_ALERT "Hello, world\n"); //printk je funkcija koja omogucava stampanje na konzoli, ona je neophodna jer drajver iz kernela nema pristup c bibliotekama.
+                                        //KERN_ALERT je makro koji ispisuje prioritet poruke 
+    return 0;
+}
+
+static void __init hello_exit(void)
+{
+    printk(KERN_ALERT "Goodbye, cruel world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+//_exit i _init su makroi koji definisu ulogu fukcije
